43135b165d6affd45ef2e193ebec43f89d689908
Tienan Li <litienan993@gmail.com>
2015-12-07 07:36:21 -0800

    Update LinRegTest.m

--- Linear-Regression/LinRegTest.m
+++ Linear-Regression/LinRegTest.m
@@ -9,11 +9,12 @@ load('RandomData.mat')
 % errror1: testing error for original data fitted linear regression
 % errror1_t: training error for original data fitted linear regression
 % noise and noise2 are the noise for the TrainX and TestX
+lambda=10;
 noise=randn(size(TrainX))*10^-3;
 noise2=randn(size(TestX))*10^-3;
 trainX=TrainX+noise;
 testX=TestX+noise2;
-model=linregFit(trainX,TrainY);
+model=linregFit(trainX,TrainY,'regType','L2','lambda',lambda);
 res=round(linregPredict(model,testX));
 res_t=round(linregPredict(model,trainX));
 er1=zeros(1,length(res));
@@ -32,27 +33,13 @@ error1_t=(sum(er1_t))/length(res_t)
 % testing and training standard error.
 % errror2: testing error for normalized data fitted linear regression
 % errror2_t: training error for normalized data fitted linear regression
-[r,c]=size(trainX);
-y=zeros(r,c);
-for i=1:c
-    v=trainX(:,i);
-    m=mean(v);
-    s=std(v);
-    y(:,i)=(v-m)/s;
-end
+trainX_n=standardizeCols(TrainX);
+testX_n=standardizeCols(TestX);
 
-[r2,c2]=size(testX);
-y2=zeros(r2,c2);
-for k=1:c2
-    v2=testX(:,k);
-    m2=mean(v2);
-    s2=std(v2);
-    y2(:,k)=(v2-m2)/s2;
-end
 
-model2=linregFit(y,TrainY);
-res2=round(linregPredict(model2,y2));
-res2_t=round(linregPredict(model2,y));
+model2=linregFit(trainX_n,TrainY,'regType','L2','lambda',lambda);
+res2=round(linregPredict(model2,testX_n));
+res2_t=round(linregPredict(model2,trainX_n));
 
 er2=zeros(1,length(res2));
 for i=1:length(res2);
